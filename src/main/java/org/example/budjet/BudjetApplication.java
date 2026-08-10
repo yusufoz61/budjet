8d1f7c83f220873747bacb6b6400ccaa2f6ee7f8
yusuf <yusufoz61@outlook.com>
2026-08-10 10:03:06 +0200

    bootup

---
+++ src/main/java/org/example/budjet/BudjetApplication.java
@@ -0,0 +1,13 @@
+package org.example.budjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudjetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/budjet/BudjetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.budjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudjetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
